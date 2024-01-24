d9ad6afc76eca46b56dbea2894aec33a7be0d52c
John Schiltz <45466247+schiltz3@users.noreply.github.com>
2024-01-23 22:51:18 -0600

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use serialport::{available_ports, SerialPortInfo, SerialPortType, UsbPortInfo};
+fn main() {
+    match available_ports() {
+        Ok(ports) => {
+            match ports.len() {
+                0 => println!("No Ports Found"),
+                1 => println!("1 Port FOund"),
+                _ => println!("Many ports found"),
+            }
+            for port in ports {
+                println!("{}", port.port_name);
+                match port.port_type {
+                    SerialPortType::UsbPort(_usbinfo) => {
+                        println!(" Usb Type: ");
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+        }
+    }
+}
