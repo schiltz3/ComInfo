31adf55d61e80f989baf253b324d907ec28a80cb
John Schiltz <45466247+schiltz3@users.noreply.github.com>
2024-01-24 00:50:03 -0600

    Auto reload

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
-use serialport::{available_ports, SerialPortType};
+use console::Term;
+use rusb;
+use serialport::{available_ports, SerialPortInfo, SerialPortType};
 fn main() {
-    match available_ports() {
-        Ok(ports) => {
-            match ports.len() {
-                0 => println!("No Ports Found"),
-                1 => println!("1 Port FOund"),
-                _ => println!("Many ports found"),
-            }
-            for port in ports {
-                println!("{}", port.port_name);
-                match port.port_type {
-                    SerialPortType::UsbPort(_usbinfo) => {
-                        println!(" Usb Type: ");
-                    }
-                    _ => {}
+    let term = Term::stdout();
+    term.set_title("Serial List");
+
+    println!("Support hotplug?: {}", rusb::has_hotplug());
+    // context = Context::new();
+    // context.has_hotplug;
+
+    let mut previous_num = 0;
+    loop {
+        match available_ports() {
+            Ok(ports) => {
+                if ports.len() != previous_num {
+                    previous_num = ports.len();
+                    print_ports(ports, &term);
                 }
             }
+            Err(e) => {
+                println!("{:?}", e);
+            }
         }
-        Err(e) => {
-            println!("{:?}", e);
+    }
+}
+
+fn print_ports(ports: Vec<SerialPortInfo>, term: &Term) {
+    // term.move_cursor_to(0, 0);
+    term.clear_screen();
+    for port in ports {
+        print!("{} - ", port.port_name);
+        match port.port_type {
+            SerialPortType::UsbPort(usbinfo) => {
+                println!("\tProduct: {}", usbinfo.product.clone().unwrap_or_default());
+                println!(
+                    "\tManufacturer: {}",
+                    usbinfo.manufacturer.clone().unwrap_or_default()
+                );
+                println!("\tPid: {}", usbinfo.pid);
+                println!(
+                    "\tSerial Number: {}",
+                    usbinfo.serial_number.clone().unwrap_or_default()
+                );
+            }
+            _ => {}
         }
     }
 }
