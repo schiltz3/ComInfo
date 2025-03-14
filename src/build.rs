13f0400d346c673d10e54398d0fecb17c7956490
schiltz3 <johnschiltz2000@gmail.com>
2025-03-14 11:19:26 -0500

    modify build script to pass build version to installer compiler

--- src/build.rs
+++ src/build.rs
@@ -3,21 +3,37 @@
 use std::env;
 use std::process::Command;
 
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo::warning={}", format!($($tokens)*))
+    }
+}
+
 fn main() {
     // Get the current profile from the environment
     let profile = env::var("PROFILE").unwrap_or_else(|_| "debug".to_string());
+    let version = env!("CARGO_PKG_VERSION");
 
     if profile == "release" {
         let output = Command::new("iscc")
             .arg("./InstallerScripts/ComiInstallerScript.iss")
+            .arg(format!("/DMyAppVersion={}", version))
             .output()
-            .expect("Failed to execute post installer compiler");
+            .expect("Failed to execute post build installer compiler");
 
-        // Optionally, handle the output
-        println!("Output: {:?}", String::from_utf8_lossy(&output.stdout));
-        println!("Error: {:?}", String::from_utf8_lossy(&output.stderr));
+        // Handle the output if the command succeeded
+        String::from_utf8_lossy(&output.stdout)
+            .lines()
+            .for_each(|line| {
+                p!("{}", line);
+            });
+
+        String::from_utf8_lossy(&output.stderr)
+            .lines()
+            .for_each(|line| {
+                p!("{}", line);
+            });
     } else {
-        println!("Skipping command execution. Not in release mode.")
+        p!("Skipping command execution. Not in release mode.")
     }
-    println!("Skipping command execution. Not in release mode.")
 }
